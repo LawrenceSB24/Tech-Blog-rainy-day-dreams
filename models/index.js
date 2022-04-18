a3d60054be47da61fb5d3cf7d1dbb66bdb3fb528
LawrenceSB24 <lawrs2022@gmail.com>
2022-04-17 20:51:15 -0400

    Added onDelete option for models

--- models/index.js
+++ models/index.js
@@ -5,32 +5,38 @@ const Comment = require('./comment');
 
 // Each user hasMany posts
 User.hasMany(Posts, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
 });
 
 // Posts belongsTo User
 Posts.belongsTo(User, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
 });
 
 // Each post hasMany comments
 Posts.hasMany(Comment, {
-    foreignKey: 'post_id'
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
 });
 
 // Comments for posts belongsTo that post
 Comment.belongsTo(Posts, {
-    foreignKey: 'post_id'
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
 });
 
 // User hasMany comments (users can have many comments)
 User.hasMany(Comment, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
 });
 
 // Comments belongTo each user 
 Comment.belongsTo(User, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
 });
 
 // Exports all models for external use
